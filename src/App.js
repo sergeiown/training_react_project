fdd2d54ce78cfcd3a43e58b39a3367230de08777
Serhii Myshko <sergeiown@gmail.com>
2023-02-18 19:32:17 +0200

    some code format changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React, {useState} from "react";
-import "./styles/App.css";
-import PostList from "./components/PostList";
-import PostForm from "./components/PostForm";
+import React, { useState } from 'react';
+import './styles/App.css';
+import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 function App() {
-	const [posts, setPosts] = useState([
-		{id: 1, title: "Javascript ES5", body: "Description"},
-		{id: 2, title: "Javascript ES6", body: "Description"},
-		{id: 3, title: "Javascript ES7", body: "Description"},
-	]);
-	
-	const createPost = (newPost) => {
-		setPosts([...posts, newPost]);
-	};
-	
-	return (
-		
-		<div className="App">
-			
-			<PostForm create={createPost}></PostForm>
-			<PostList posts={posts} title={"Посты про JS"}></PostList>
-		
-		</div>
-	
-	);
+    const [posts, setPosts] = useState([
+        { id: 1, title: 'Javascript ES5', body: 'Description' },
+        { id: 2, title: 'Javascript ES6', body: 'Description' },
+        { id: 3, title: 'Javascript ES7', body: 'Description' },
+    ]);
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost]);
+    };
+
+    return (
+        <div className='App'>
+            <PostForm create={createPost}></PostForm>
+            <PostList posts={posts} title={'Посты про JS'}></PostList>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Clicker.jsx
+++ src/components/Clicker.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 function Clicker() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  });
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    });
 
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-    </div>
-  );
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+    );
 }
 
 export default Clicker;
