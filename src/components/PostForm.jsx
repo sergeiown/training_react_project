afb43134ea88639395272353a5798b05f4196100
Serhii Myshko <sergeiown@gmail.com>
2023-02-18 17:49:47 +0200

    Update PostForm.jsx

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,15 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState} from "react";
 import MyInput from "./UI/input/MyInput";
 import MyButton from "./UI/button/MyButton";
 
 const PostForm = ({create}) => {
-	
 	const [post, setPost] = useState({title: "", body: ""});
 	
 	const addNewPost = (e) => {
 		e.preventDefault();
 		const newPost = {
-			...post, id: Date.now()
+			...post,
+			id: Date.now(),
 		};
 		create(newPost);
 		setPost({title: "", body: ""});
@@ -17,7 +17,6 @@ const PostForm = ({create}) => {
 	
 	return (
 		<form>
-			
 			<MyInput
 				value={post.title}
 				onChange={(e) => setPost({...post, title: e.target.value})}
